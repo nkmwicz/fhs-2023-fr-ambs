6bf8b835f15cccf9a4df91ebf263d8dc2ba9d65b
Nathan Michalewicz <77887856+nkmwicz@users.noreply.github.com>
2023-03-01 20:16:06 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="d-flex-center">
+        <h1>Hello World!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
